d41db312ef9c9e7caf5347b7e8f9e289d728e83d
Pawel Borkar <pawel@duck.com>
2023-10-24 13:46:35 +0530

    Add: User Schema

--- server/src/models/User.models.js
+++ server/src/models/User.models.js
@@ -1,37 +1,55 @@
 import { Schema, model } from 'mongoose';
 
-const UserSchema = Schema({
-  fullname: {
-    type: String,
-    required: [true, 'Please enter a full name.'],
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
+const UserSchema = Schema(
+  {
+    avatar: {
+      type: {
+        url: String,
+        localPath: String,
+      },
+      default: {
+        url: 'https://via.placeholder.com/200x200.png',
+        localPath: '',
+      },
+    },
+    fullname: {
+      type: String,
+      required: [true, 'Please enter a full name.'],
+      lowercase: true,
+      trim: true,
+    },
+    username: {
+      type: String,
+      required: [true, 'Please enter a full name.'],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        'Please add a valid email',
+      ],
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Please enter a password'],
+      minlength: 8,
+      select: false,
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  email: {
-    type: String,
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      'Please add a valid email',
-    ],
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: [true, 'Please enter a password'],  
-    minlength: 8,
-    select: false,
-  },
-  resetPasswordToken: String,
-  resetPasswordExpire: Date,
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 
 export default model('User', UserSchema);
