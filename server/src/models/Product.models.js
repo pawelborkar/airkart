d8f4acf8797456ae2a43b62dbcacecbabf0c67e5
Pawel Borkar <pawel@duck.com>
2023-10-24 13:46:51 +0530

    Add: Product schema

--- server/src/models/Product.models.js
+++ server/src/models/Product.models.js
@@ -1,30 +1,64 @@
 import { Schema, model } from 'mongoose';
+import slugify from 'slugify';
 
-const ProductSchema = new Schema({
-  name: {
-    type: String,
-    require: [true, 'Please enter the product name.'],
-    trim: true,
-    maxlength: 255,
+const ProductSchema = new Schema(
+  {
+    name: {
+      type: String,
+      require: [true, 'Please enter the product name.'],
+      trim: true,
+      maxlength: 127,
+    },
+    slug: String,
+    inStock: {
+      type: Boolean,
+      require: [true, 'Please add the available of the product.'],
+    },
+    category: {
+      type: String,
+      require: [
+        true,
+        'Please enter the category to which the product belongs.',
+      ],
+      maxlength: 50,
+    },
+    price: {
+      type: Number,
+      require: [true, 'Please enter the price of the product'],
+      trim: true,
+      maxlength: 6,
+    },
+    description: {
+      type: String,
+      require: [true, 'Please add a description of the product.'],
+      trim: true,
+      maxlength: 500,
+    },
+    averageRating: {
+      type: Number,
+      min: [1, 'Rating must be at least 1'],
+      max: [5, 'Rating must can not be more than 5'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    // user: {
+    //   type: mongoose.Schema.ObjectId,
+    //   ref: 'User',
+    //   required: true,
+    // },
   },
-  category: {
-    type: String,
-    require: [true, 'Please enter the category to which the product belongs.'],
-    maxlength: 50,
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   },
-  price: {
-    type: Number,
-    require: [true, 'Please enter the price of the product'],
-    trim: true,
-  },
-  description: {
-    type: String,
-    require: [true, 'Please add a description of the product.'],
-    trim: true,
-  },
-  rating: {
-    type: Number,
-  },
+  { timestamps: true }
+);
+
+// Create slug for url
+ProductSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
-
 export default model('Product', ProductSchema);
