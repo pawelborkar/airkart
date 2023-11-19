c6c6238a6a8f2a21fdd09d0224df0cec6115be65
Pawel Borkar <pawel@duck.com>
2023-11-19 15:24:18 +0530

    feat: cart schema

---
+++ server/src/models/cart.models.js
@@ -0,0 +1,37 @@
+import { model, Schema } from 'mongoose';
+import { Product } from '../models/product.models.js';
+
+const cartItemSchema = new Schema({
+  product: {
+    type: Schema.Types.ObjectId,
+    ref: 'Product',
+    require: [true, 'Product required.'],
+  },
+  quantity: {
+    type: Number,
+    default: 1,
+    required: [true, 'Please add atleast one unit'],
+  },
+});
+
+const cartSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    items: [cartItemSchema],
+    total: {
+      type: Number,
+      default: 0,
+    },
+  },XMLDocument
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+  { timestamps: true }
+);
+
+export default model('Cart', cartSchema);
