1e1ea6bebdb242b21505b508e335e4fa6e547ab3
Pawel Borkar <pawel@duck.com>
2024-03-05 19:38:12 +0530

    resize height on small screens

--- client/components/OfferBanner/OfferBanner.tsx
+++ client/components/OfferBanner/OfferBanner.tsx
@@ -4,11 +4,11 @@ import { Card, Image } from '@nextui-org/react';
 
 const OfferBanner = () => {
   return (
-    <Card className="my-4">
+    <Card className="my-4 h-[10vh] md:h-full">
       <Image
         alt="offer-banner"
         width={'100%'}
-        height={160}
+        className="scale-125 md:scale-100 h-[10vh]"
         src="https://rukminim1.flixcart.com/fk-p-flap/1600/140/image/e3fab105530a540f.jpg?q=20"
       />
     </Card>
