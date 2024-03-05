7198eab2704557fcd62a00d3b2853e96ed236dc5
Pawel Borkar <pawel@duck.com>
2024-03-05 19:42:01 +0530

    feat: make mobile responsive

--- client/app/products/[productId]/page.tsx
+++ client/app/products/[productId]/page.tsx
@@ -8,7 +8,7 @@ import { IProductDetails, IProductParams, IProductsResult } from '@/interfaces';
 import ProductCarousel from '@/components/ProductCarousel/ProductCarousel';
 
 const ProductDetails = ({ params }: IProductParams) => {
-  const isInCart = true;
+  const isInCart = false;
 
   const productId = params.productId;
   const { data, isError, isLoading } = useQuery({
@@ -37,20 +37,11 @@ const ProductDetails = ({ params }: IProductParams) => {
     });
   }
   const product = data?.products;
-  const {
-    name,
-    price,
-    description,
-    brand,
-    category,
-    countryOfOrigin,
-    stock,
-    imageURLs,
-  } = product;
+  const { name, price, description, brand, category, countryOfOrigin, stock, imageURLs } = product;
   return (
     <>
       <Card className="flex flex-col md:flex-row p-4 m-4 items-center justify-evenly">
-        <div className="w-full md:h-[60vh] flex justify-center items-center mb-4 md:mb-0">
+        <div className="w-full md:h-2/6 flex justify-center items-center mb-4 md:mb-0">
           <Image
             height={680}
             width={680}
@@ -60,41 +51,41 @@ const ProductDetails = ({ params }: IProductParams) => {
             src={imageURLs[0]}
           />
         </div>
-        <div className="w-full h-[42vh] md:w-2/4 flex flex-col justify-evenly md:m-4 p-4">
+        <div className="w-full md:w-2/4 h-4/6  flex flex-col justify-around md:m-4 p-4">
           <div className="h-1/5">
-            <p className="text-3xl">&#8377; {price}</p>
+            <p className="text-3xl py-2"> {name} </p>
+            <p className="text-2xl text-green-500 font-medium">&#8377;{price}</p>
           </div>
-          <div className="text-xl mt-2 mb-4 h-2/5 ">{description}</div>
-          <div className="flex flex-col justify-center border-1 items-center md:w-full md:h-24 mt-6 p-2">
+          <div className="text-xl mt-2 mb-4 h-1/5">{description}</div>
+          <div className="h-3/5 flex flex-col justify-evenly items-center md:w-full p-2">
             {stock > 0 ? (
-              <>
-                <Button
-                  radius="full"
-                  fullWidth
-                  size="lg"
-                  color="warning"
-                  startContent={<ShoppingCart />}
-                >
-                  {isInCart ? 'Remove from cart' : 'Add to Cart'}
-                </Button>
-                <Button
-                  startContent={<Zap />}
-                  radius="full"
-                  className="bg-gradient-to-tr from-pink-500 to-orange-500 text-white shadow-lg"
-                  fullWidth
-                  size="lg"
-                >
-                  {isInCart ? 'Go to cart' : 'Buy Now'}
-                </Button>
-              </>
+              <div className="w-full">
+                <div className="w-full mb-2">
+                  <Button
+                    radius="full"
+                    fullWidth
+                    size="lg"
+                    color="warning"
+                    startContent={<ShoppingCart />}
+                  >
+                    {isInCart ? 'Remove from cart' : 'Add to Cart'}
+                  </Button>
+                </div>
+
+                <div>
+                  <Button
+                    startContent={<Zap />}
+                    radius="full"
+                    className="bg-gradient-to-tr from-pink-500 to-orange-500 text-white shadow-lg"
+                    fullWidth
+                    size="lg"
+                  >
+                    {isInCart ? 'Go to cart' : 'Buy Now'}
+                  </Button>
+                </div>
+              </div>
             ) : (
-              <Button
-                radius="full"
-                fullWidth
-                size="lg"
-                color="warning"
-                variant="faded"
-              >
+              <Button radius="full" fullWidth size="lg" color="warning" variant="faded">
                 Out of Stock
               </Button>
             )}
